1f47e9009856fe311060a3425ae70c2cd8c91028
Michał Szałapski <michal@szalapski.pl>
2018-09-18 17:32:06 +0200

    test

--- classes/class.MazeAnalyzer.php
+++ classes/class.MazeAnalyzer.php
@@ -15,7 +15,6 @@ class MazeAnalyzer extends MazeSearcher
     }
   public function getCostOfCreating($aWallPrice, $aFieldPrice, $aTorchPrice)
     {
-<<<<<<< HEAD
     $this->checkIfPriceIsNumberOrThrowException($aWallPrice);
     $this->checkIfPriceIsNumberOrThrowException($aFieldPrice);
     $this->checkIfPriceIsNumberOrThrowException($aTorchPrice);
@@ -41,88 +40,60 @@ class MazeAnalyzer extends MazeSearcher
     $isNotIntVal = $intVal === 0;
 
     if($isNotFloatVal or $isNotIntVal)
-=======
-	$this->checkIfPriceIsNumberOrThrowException($aWallPrice);
-	$this->checkIfPriceIsNumberOrThrowException($aFieldPrice);
-	$this->checkIfPriceIsNumberOrThrowException($aTorchPrice);
-
-	$this->countWallsAndfields();
-	$overAllWallPrice = $this->walls * $aWallPrice;
-	$overAllFieldPrice = $this->fields * $aFieldPrice;
-
-	$this->numberOfCorridors = $this->getMazeNumberOfCorridors();
-
-	if($this->numberOfCorridors % 2 == 1)
-	  $overAllTorchPrice = (($this->numberOfCorridors-1)/2)*$aTorchPrice;
-	else
-	  $overAllTorchPrice = $this->numberOfCorridors*$aTorchPrice;
-	return round($overAllTorchPrice+$overAllWallPrice+$overAllFieldPrice, 2);
-	}
-  private function checkIfPriceIsNumberOrThrowException($aValue)
-    {
-	$floatVal = floatval($aValue);
-	$isNotFloatVal = $floatVal === 0;
-
-	$intVal = intval($aValue);
-	$isNotIntVal = $intVal === 0;
-
-	if($isNotFloatVal or $isNotIntVal)
->>>>>>> 0ec079c987f685fa9c9aae28723cb6d33ed3f8aa
-	  throw new Exception("Price isn't float or int.",400);
-	}
+      throw new Exception("Price isn't float or int.",400);
+    }
   public function getExitCords()
     {
-	$roadToExit = $this->findExitOfMaze();
-	$lastfieldPosition = count($roadToExit)-1;
-	$exitCords = array($roadToExit[$lastfieldPosition][0],$roadToExit[$lastfieldPosition][1]);
-	return $exitCords;
-	}
+    $roadToExit = $this->findExitOfMaze();
+    $lastfieldPosition = count($roadToExit)-1;
+    $exitCords = array($roadToExit[$lastfieldPosition][0],$roadToExit[$lastfieldPosition][1]);
+    return $exitCords;
+  	}
 
   public function getNumberOfWalls()
     {
-	return $this->walls;
-	}
+    return $this->walls;
+    }
   public function getNumberOffields()
     {
-	return $this->fields;
-	}
+    return $this->fields;
+    }
 
   public function countWallsAndfields()
     {
-	$this->checkIfMazeStructureExistOrThrowException();
-	$numberOfWalls = 0;
-	$numberOffields = 0;
+    $this->checkIfMazeStructureExistOrThrowException();
+    $numberOfWalls = 0;
+    $numberOffields = 0;
     foreach($this->mazeStructure as $xAxisArray)
-	  {
-	  foreach($xAxisArray as $rowValue)
-	    {
-		if($rowValue === 1)
-		  $numberOfWalls++;
-		elseif($rowValue === 0)
-		  $numberOffields++;
-		}
-	  }
+      {
+      foreach($xAxisArray as $rowValue)
+        {
+        if($rowValue === 1)
+          $numberOfWalls++;
+        elseif($rowValue === 0)
+          $numberOffields++;
+        }
+      }
 
-	$this->walls = $numberOfWalls;
-	$this->fields = $numberOffields;
-	}
+    $this->walls = $numberOfWalls;
+    $this->fields = $numberOffields;
+    }
 
   //Maze validating
   public function setAndValidMazeStructure($aMazeStructure)
     {
-
-	MazeValidator::validMazeOrThrowException($aMazeStructure);
-	$this->mazeStructure = $aMazeStructure;
-	}
+    MazeValidator::validMazeOrThrowException($aMazeStructure);
+    $this->mazeStructure = $aMazeStructure;
+    }
   protected function checkIfMazeStructureExistOrThrowException()
     {
-	if($this->mazeStructure == NULL)
-	  throw new Exception("Maze aren't set.",400);
-	}
+    if($this->mazeStructure == NULL)
+      throw new Exception("Maze aren't set.",400);
+    }
   public function setUpEntrance($aX, $aY)
     {
-	MazeValidator::validEntranceOrThrowException($this->mazeStructure, $aX, $aY);
-	$this->entranceCordX = $aX;
-	$this->entranceCordY = $aY;
-	}
+    MazeValidator::validEntranceOrThrowException($this->mazeStructure, $aX, $aY);
+    $this->entranceCordX = $aX;
+    $this->entranceCordY = $aY;
+    }
   }
